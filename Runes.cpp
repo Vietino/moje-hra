81be4090c38332cafb35b0d46bae3a24c0f5dfa7
Vietino <duc.nguyen@skolakrizik.cz>
2025-05-11 21:12:24 +0200

    Upravy bitvy

--- Runes.cpp
+++ Runes.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 using namespace std;
-void statyassasina () {
+void statyassasina (){
 cout << " Class: Assasin" <<endl;
 cout << " Statistiky :" << endl ;
 cout << "Zivoty: 4/4" << endl ;
 cout << "Utok: 5" << endl ;
 cout << "Energie: 4/4" << endl ;
 cout << " Schopnosti:" << endl ;
-cout << "1. Rychly utok - 5 dmg " << endl ;
-cout << "2. Utok ze zadu - 10 dmg ( 2 energie )"<< endl ;
+cout << "1. Rychly utok - 5 dmg ( 1 energie )" << endl ;
+cout << "2. Utok ze zadu - 7 dmg ( 2 energie )"<< endl ;
 }
 
-void statylukostrelce ()  {
+void statylukostrelce (){
 cout << " Class: Lukostrelec" <<endl;
 cout << " Statistiky :" << endl ;
 cout << "Zivoty: 4/4" << endl ;
 cout << "Utok: 5" << endl ;
 cout << "Energie: 6/6" << endl ;
 cout << " Schopnosti:" << endl ;
-cout << "1. Strela - 4 dmg " << endl ;
+cout << "1. Strela - 4 dmg ( 1 energie) " << endl ;
 cout << "2. trojita strela - 3 dmg vsem ( 2 energie )" << endl ;
 cout << "3. Vzdalena strela - 7 dmg ( 4 energie )" << endl ;
 }
@@ -34,7 +34,7 @@ cout << "1. Mlat mecem - 3 dmg" << endl ;
 cout << "2. Stit - Snizi dmg ( 2 energie )" << endl;
 }
 
-void statymaga () {
+void statymaga (){
 
 cout << " Class: Mag" << endl ;
 cout << " Statistiky :" << endl ;
@@ -103,43 +103,110 @@ void vesnice(int& zivoty, int& maxzivoty, int& energie, int& maxenergie, int& ut
         }
     }
 }
-void bitvamonstrum1 (int &zivoty, int &energie, int &utok) {
-int zivotymonstra = 6;
-int utokmonstra = 2;
-int akce ;
+void bitvamonstrum1(int classa, int &zivoty, int &energie, int utok, int &zlato, int &exp){
+    int zivotyMonstra = 10;
+    int utokMonstra = 3;
+    int volba;
+    bool omraceno = false;
+
+    cout << endl << "Narazil jsi na zakerne monstrum!" << endl;
+
+    while (zivoty > 0 && zivotyMonstra > 0){
+        cout << endl << "Tve zivoty: " << zivoty << " | Energie/Mana: " << energie << endl;
+        cout << "Zivoty monstra: " << zivotyMonstra << endl;
+
+        if (classa == 1){
+            cout << "1. Rychly utok - 5 dmg (1 energie)" << endl;
+            cout << "2. Utok ze zadu - 10 dmg (2 energie)" << endl;
+        } else if (classa == 2){
+            cout << "1. Strela - 4 dmg (1 energie)" << endl;
+            cout << "2. Trojita strela - 3 dmg (2 energie)" << endl;
+            cout << "3. Vzdalena strela - 7 dmg (4 energie)" << endl;
+        } else if (classa == 3){
+            cout << "1. Mlat mecem - 3 dmg (1 energie)" << endl;
+            cout << "2. Stit - snizi dmg (2 energie)" << endl;
+        } else if (classa == 4){
+            cout << "1. Ohniva strela - 4 dmg (2 mana)" << endl;
+            cout << "2. Poryv vetru - 7 dmg (4 mana)" << endl;
+            cout << "3. Ledove kouzlo - omraci monstrum (4 mana)" << endl;
+        }
+
+        cout << "4. Odpocivat (+1 energie/mana)" << endl;
+        cin >> volba;
+
+        int dmg = 0;
 
 
-cout << " Narazil jsi na male monstrum!" << endl;
+        if (classa == 1){
+            if (volba == 1 && energie >= 1){
+                energie--;
+                dmg = 5;
+            } else if (volba == 2 && energie >= 2){
+                energie -= 2;
+                dmg = 10;
+            }
+        } else if (classa == 2){
+            if (volba == 1 && energie >= 1){
+                energie--;
+                dmg = 4;
+            } else if (volba == 2 && energie >= 2){
+                energie -= 2;
+                dmg = 3;
+            } else if (volba == 3 && energie >= 4){
+                energie -= 4;
+                dmg = 7;
+            }
+        } else if (classa == 3) {
+            if (volba == 1 && energie >= 1){
+                energie--;
+                dmg = 3;
+            } else if (volba == 2 && energie >= 2){
+                energie -= 2;
+                utokMonstra--;
+                cout << "Pouzil jsi stit! Utok monstra se snizuje." << endl;
+            }
+        } else if (classa == 4) {
+            if (volba == 1 && energie >= 2) {
+                energie -= 2;
+                dmg = 4;
+            } else if (volba == 2 && energie >= 4) {
+                energie -= 4;
+                dmg = 7;
+            } else if (volba == 3 && energie >= 4) {
+                energie -= 4;
+                omraceno = true;
+                cout << "Seslal jsi ledove kouzlo! Monstrum je omraceno." << endl;
+            }
+        }
 
-while (zivoty > 0 && zivotymonstra > 0 ){
-    cout << "Tve zivoty: " << zivoty << ", Energie: " << energie << endl ;
-    cout << " Zivoty monstra: " << zivotymonstra << endl ;
-    cout << " 1 - utocit " << endl ;
-    cout << " 2 - odpocivat (+1 energie )" << endl ;
-    cin >> akce ;
+        if (volba == 4) {
+            energie++;
+            cout << "Odpocivas." << endl;
+        } else if (dmg > 0) {
+            zivotyMonstra -= dmg;
+            cout << "Zpusobil jsi " << dmg << " poskozeni!" << endl;
+        } else if (volba != 4 && !omraceno) {
+            cout << "Nemas dost energie/many nebo neplatna volba." << endl;
+        }
 
-if ( akce == 1 && energie > 0 ){
-    zivotymonstra -= utok ;
-    energie--;
-    cout << " Zautocil jsi a zpusobil " << utok << "poskozeni!" << endl ;
-}else if (akce == 2){
-energie++;
-cout << " Odpocivas, +1 energie " << endl ;
-}else {
-cout << " Nemas dost energie nebo neplatna volba " << endl ;
-}
-
-if (zivotymonstra >0) {
-    zivoty -= utokmonstra ;
-    cout << "Monstrum te zranilo za " << utokmonstra << " zivotu!" << endl ;
+        if (zivotyMonstra > 0 && !omraceno) {
+            zivoty -= utokMonstra;
+            cout << "Monstrum te zasahlo za " << utokMonstra << "!" << endl;
+        } else if (omraceno) {
+            cout << "Monstrum je omraceno a neutoci tento tah!" << endl;
+            omraceno = false;
+        }
     }
-}if (zivoty >0) {
-cout << " Zvitezil jsi nad monstrem! " << endl ;
-}else {
-cout << " Byl jsi porazen..." << endl ;
 
+    if (zivoty > 0) {
+        cout << "Zvitezil jsi!" << endl;
+        zlato += 10;
+        exp += 5;
+    } else {
+        cout << "Byl jsi porazen..." << endl;
+    }
 }
-}
+
 int main(){
 int volba = 0 ;
 int maxzivoty = 0 ;
@@ -148,7 +215,8 @@ int maxenergie = 0 ;
 int energie = 0 ;
 int utok = 0 ;
 int zlato = 50 ;
-
+int classa = 0;
+int exp = 0;
 while (true) {
     cout << " Vitej ve hre Runes" << endl ;
     cout << " vyber si classu:" << endl ;
@@ -160,24 +228,28 @@ while (true) {
 
 switch (volba) {
 case 1 :
+    classa = 1 ;
     statyassasina() ;
     zivoty=maxzivoty=4;
     energie=maxenergie=4;
     utok=5;
     break ;
 case 2 :
+    classa = 2 ;
     statylukostrelce();
     zivoty=maxzivoty=4;
     energie=maxenergie=5;
     utok=6;
     break ;
 case 3 :
+    classa = 3;
     statyrytire();
     zivoty=maxzivoty=7;
     energie=maxenergie=3;
     utok=4;
     break ;
 case 4:
+    classa = 4 ;
     statymaga();
     zivoty=maxzivoty=4;
     energie=maxenergie=8;
@@ -207,8 +279,7 @@ switch (volba) {
     case 1 : vesnice(zivoty, maxzivoty, energie, maxenergie, utok, zlato);
     break ;
     case 2 :
-        bitvamonstrum1 (zivoty, energie, utok);
-
+         bitvamonstrum1 (classa, zivoty, energie, utok, zlato, exp);
         break ;
     case 3 :
         break ;
